37e8f4c5e2950db9ed358accc7516a35dbe2f6a7
simonacorreale <simona.correale.work@gmail.com>
2024-12-11 18:24:24 +0100

    feat: fix

--- script.js
+++ script.js
@@ -15,8 +15,31 @@ Buon lavoro!*/
 //3) Dopo i controlli faccio si che il prezzo del biglietto venga calcolato moltiplicando la distanza per il prezzo base
 //4) Dopo di che inserisco un else dove a sua volta verrà inserito un if all'interno per creare la condizione che soddisfa lo sconto uder 18 e over 65
 //5) Dopo di che i numeri che saranno il risultato devono essere scritti secondo la regola "umana" quindi ben definiti tramite i due decimali dopo la virgola
+//6) Mostro il risultato
 
+// Punto 1
+const distance = parseFloat(prompt("Quanti chilometri vuoi percorrere?"));
+const age = parseInt(prompt("Definisci la tua età 👵🏻👩🏻"));
+// Punto 2
+if (isNaN(distance) || isNaN(age) || distance <= 0 || age <= 0) {
+    alert("Errore: la tua età o il numero di KM percorsi non possono corrispondere a zero 🤯 "); 
+} else {
+// Punto 3
+    const priceKm = 0.21;
+    let totalPrice = distance * priceKm; 
+// Punto 4
+    if (age < 18) {
+        totalPrice *= 0.8;
+    } else if (age >= 65) {
+        totalPrice *= 0.6; 
+    }
+ // Punto 5
+    const finalPrice = totalPrice.toFixed(2);
 
+ // Punto 6
+    alert(`Il tuo prezzo è: €${finalPrice}`);
+    console.log(`Il tuo prezzo è €${finalPrice}`);
+}
 
 
 
